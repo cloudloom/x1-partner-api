e1fe1b422c3cc011e8dc5fa31dcdd105dc9c3871
Vishwajit <vishwajit.bhadrashetty@mmpsd.com>
2015-06-01 18:45:53 +0530

    few more changes made to PartnerControllerTest. (Latest Changes)

--- src/test/java/com/tracebucket/x1/partner/api/test/controller/PartnerControllerTest.java
+++ src/test/java/com/tracebucket/x1/partner/api/test/controller/PartnerControllerTest.java
@@ -10,6 +10,7 @@ import com.tracebucket.x1.partner.api.rest.resources.DefaultPartnerRoleResource;
 import com.tracebucket.x1.partner.api.test.fixture.DefaultAddressResourceFixture;
 import com.tracebucket.x1.partner.api.test.fixture.DefaultAffiliateResourceFixture;
 import com.tracebucket.x1.partner.api.test.fixture.DefaultPartnerResourceFixture;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,9 +21,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Set;
 
 /**
@@ -170,5 +174,17 @@ public class PartnerControllerTest {
         Assert.assertNotNull(partner.getUid());
         Assert.assertNotNull(partner.getAffiliate());
         Assert.assertNotNull(partner.getAffiliate().getUid());
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        if(partner != null && partner.getUid() != null) {
+            restTemplate.delete(basePath + "/partner/" + partner.getUid());
+            try {
+                restTemplate.getForEntity(new URI(basePath + "/partner/" + partner.getUid()), DefaultPartnerResource.class);
+            } catch (HttpClientErrorException httpClientErrorException) {
+                Assert.assertEquals(HttpStatus.NOT_FOUND, httpClientErrorException.getStatusCode());
+            }
+        }
     }
 }
